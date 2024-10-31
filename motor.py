fdbf16c77217563250adfccf872fcc0855b8b132
keenanl00 <keenanl@utexas.edu>
2024-10-31 15:42:20 -0500

    Add files via upload
    Finished with the necessary files. Just need to change code for accessing file location

--- motor.py
+++ motor.py
@@ -1,5 +1,7 @@
-from rocketpy import SolidMotor
-import csv
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+from rocketpy import Environment, Flight, Function, Rocket, SolidMotor
+import pandas as pd
 import numpy as np
 
 data_entry = input("If you want to manually enter Rocket Motor info enter 1, If you want to upload a CSV enter 0: ")
@@ -31,6 +33,9 @@ if data_entry == 1:
     motor_position = input("Enter the motor position(m): ")
     motor_position_unc = input("Enter the motor position uncertainty: ")
     grain_number = input("Enter the grain number: ")
+    center_of_dry_mass_position = input("Enter the center of dry mass position: ")
+    center_of_dry_mass_position_unc = input("Enter the uncertainty in the center of dry mass position: ")
+
     parameters = {
         # propulsion details
         "motor_structure_mass": (motor_structure_mass, motor_structure_mass_unc),
@@ -45,31 +50,17 @@ if data_entry == 1:
         "grains_center_of_mass_position": (grains_center_of_mass_position, grains_center_of_mass_position_unc),
         "nozzle_position": (nozzle_position, nozzle_position_unc),
         "motor_position": (motor_position, motor_position_unc),
-        "grain_number": (grain_number)
+        "grain_number": (grain_number),
+        "center_of_dry_mass_position": (center_of_dry_mass_position, center_of_dry_mass_position_unc)
     }
        
 if data_entry == 0:
-    csv_data_order = input("If you need to see the data order the csv needs to be in enter 1, if not, press 0")
-    csv_data_order = int(csv_data_order)
-    if csv_data_order == 1:
-        print("Ensure the data is put in this order in the csv: motor_structure_mass, motor_structure_mass_uncertainty, burn_time, burn_time_unc, nozzle_radius, nozzle_radius_unc, " +
-              "throat_radius, throat_radius_unc, grain_seperation, grain_seperation_unc, grain_density, grain_density_unc, grain_outer_radius, grain_outer_radius_unc, " +
-              "grain_initial_inner_radius, grain_initial_inner_radius_unc, grain_initial_height, grain_initial_height_unc, grains_center_of_mass_position, " +
-              "grains_center_of_mass_position_unc, nozzle_position, nozzle_position_unc, motor_position, motor_position_unc, grain_number")
-    elif csv_data_order == 0:
-        file_location = input("Please paste the full file location here: ")
-        file_data = []
-
-        #BIG PROBLEM HERE, can't access the list past [0] because all csv data is stored at [0]
-        with open(file_location, mode = 'r', encoding = 'utf-8-sig')as csvfile:
-            reader = csv.reader(csvfile, delimiter=",")
-            for line in reader:
-                file_data.append(line)
-            file_data = np.float64(file_data)       
-        print(file_data[1])     
-
+    file_location = input("Please paste the full file location here (ensure to add .csv to the file name): ")
+    df = pd.read_csv(file_location, dtype = {'data':np.float16})
+    file_data = df["data"]
+    file_data = file_data.tolist()
+        
     parameters = {
-    # propulsion details
         "motor_structure_mass": (file_data[0], file_data[1]),
         "burn_time": (file_data[2], file_data[3]),
         "nozzle_radius": (file_data[4], file_data[5]),
@@ -82,16 +73,17 @@ if data_entry == 0:
         "grains_center_of_mass_position": (file_data[18], file_data[19]),
         "nozzle_position": (file_data[20], file_data[21]),
         "motor_position": (file_data[22], file_data[23]),
-        "grain_number": (file_data[24])
+        "grain_number": (int(file_data[24])),
+        "center_of_dry_mass_position": (file_data[25], file_data[26])
     }
 
 
 Quest_Micro_Maxx_II = SolidMotor(
-    thrust_source="C:/Users/keena/Desktop/Vs Code/LRA/Quest_MICRO_MAXX_II.eng",
+    thrust_source="C:/Users/keena/Desktop/LRA/Motor Thrust Curves/AeroTech_O5500X-PS.eng",
     burn_time=parameters.get("burn_time")[0],
     dry_mass=parameters.get("motor_structure_mass")[0],
     dry_inertia=(0, 0, 0),
-    center_of_dry_mass_position=parameters.get("grains_center_of_mass_position")[0],
+    center_of_dry_mass_position=parameters.get("center_of_dry_mass_position")[0],
     grains_center_of_mass_position=parameters.get("grains_center_of_mass_position")[0],
     grain_number=parameters.get("grain_number"),
     grain_separation=parameters.get("grain_separation")[0],
